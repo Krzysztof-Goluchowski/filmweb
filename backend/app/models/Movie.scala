71e7578c629a639bfa0daa88355f7c8b58d99a71
mateuszlampert <mateuszlampert@gmail.com>
2024-05-17 23:53:15 +0200

    unified models for data fetching with JSONs

--- backend/app/models/Movie.scala
+++ backend/app/models/Movie.scala
@@ -1,13 +1,6 @@
 package models
 
-import slick.jdbc.PostgresProfile.api._
+import upickle.default.{ReadWriter}
 
-case class Movie(movieId: Int, movieName: String, averageRating: Float, category: Category)
-
-class Movies(tag: Tag) extends Table[Movie](tag, "MOVIES") {
-  def movieId = column[Int]("MOVIE_ID")
-  def movieName = column[String]("MOVIE_NAME")
-  def averageRating = column[String]("AVERAGE_RATING")
-  def category = column[String]("CATEGORY")
-  def * = (movieId, movieName, averageRating, category).mapTo[Movie]
-}
+case class Movie(movieId: Int, movieName: String, averageRating: Double, category: String)
+    derives ReadWriter
--- backend/app/models/Rating.scala
+++ backend/app/models/Rating.scala
@@ -1,13 +1,6 @@
 package models
 
-import slick.jdbc.PostgresProfile.api._
+import upickle.default.{ReadWriter}
 
-case class Rating(movieId: Int, userId: Int, stars: Int, review: Option[String]);
-
-class Ratings(tag: Tag) extends Table[Rating](tag, "RATINGS") {
-  def movieId = column[Int]("MOVIE_ID")
-  def userId = column[Int]("USER_ID")
-  def stars = column[Int]("STARS")
-  def review = column[String]("REVIEW")
-  def * = (movieId, userId, stars, review).mapTo[Rating]
-}
+case class Rating(movieId: Int, userId: Int, stars: Int, review: Option[String])
+    derives ReadWriter
