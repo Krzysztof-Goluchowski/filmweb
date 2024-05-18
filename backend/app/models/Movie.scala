5243671111b57f33099e81fcecddb932931e4bb7
mateuszlampert <mateuszlampert@gmail.com>
2024-05-18 02:37:13 +0200

    updated Movie model with short/long description

--- backend/app/models/Movie.scala
+++ backend/app/models/Movie.scala
@@ -2,5 +2,5 @@ package models
 
 import upickle.default.{ReadWriter}
 
-case class Movie(movieId: Int, movieName: String, averageRating: Double, category: String)
+case class Movie(movieId: Int, movieName: String, averageRating: Double, category: String, shortDescription: Option[String] = None, longDescription: Option[String] = None)
     derives ReadWriter
