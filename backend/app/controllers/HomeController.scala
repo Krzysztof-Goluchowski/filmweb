7e6a3b56f422d896c1266639cf7b9873ff5862ce
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2024-05-18 14:21:47 +0200

    Enchance readability of rate function

--- backend/app/controllers/HomeController.scala
+++ backend/app/controllers/HomeController.scala
@@ -57,26 +57,46 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
       val stars = (json \ "stars").as[Int]
       val review = (json \ "review").as[String]
 
-      val query = sql"SELECT average_rating, num_ratings FROM movies WHERE movie_id = $movieId".as[(Double, Int)]
-
-      db.run(query.headOption).flatMap {
-        case Some(average_rating, num_ratings) =>
-          val newNumRatings = num_ratings + 1
-          val newRating = ((num_ratings * average_rating) + stars) / newNumRatings
-
-          val updateQuery = sqlu"UPDATE movies SET average_rating = $newRating , num_ratings = $newNumRatings WHERE movie_id = $movieId"
-          val insertReviewQuery = sqlu"INSERT INTO ratings (movie_id, user_id, stars, review) VALUES ($movieId, $userId, $stars, $review)"
-
-          for {
-            _ <- db.run(updateQuery)
-            _ <- db.run(insertReviewQuery)
-          } yield Ok(Json.obj("message" -> "Added new rating and review"))
-
-        case None =>
-          Future.successful(NotFound(Json.obj("message" -> "There isn't such film!")))
+      hasAlreadyGivenRating(userId, movieId).flatMap { hasRated =>
+        if (!hasRated) {
+          postNewRating(movieId, userId, stars, review)
+        } else {
+          Future.successful(BadRequest(Json.obj("message" -> "This user already rated this film.")))
+        }
       }
     }.getOrElse {
       Future.successful(BadRequest("Expecting application/json request body"))
+    }
+  }
+
+  def hasAlreadyGivenRating(userId: Int, movieId: Int): Future[Boolean] = {
+    val query = sql"SELECT stars FROM ratings WHERE user_id = $userId AND movie_id = $movieId".as[Double]
+    db.run(query.headOption).map {
+      case Some(rating) =>
+        true
+      case _ =>
+        false
+    }
+  }
+
+  def postNewRating(movieId: Int, userId: Int, stars: Double, review: String) = {
+    val query = sql"SELECT average_rating, num_ratings FROM movies WHERE movie_id = $movieId".as[(Double, Int)]
+
+    db.run(query.headOption).flatMap {
+      case Some(average_rating, num_ratings) =>
+        val newNumRatings = num_ratings + 1
+        val newRating = ((num_ratings * average_rating) + stars) / newNumRatings
+
+        val updateQuery = sqlu"UPDATE movies SET average_rating = $newRating , num_ratings = $newNumRatings WHERE movie_id = $movieId"
+        val insertReviewQuery = sqlu"INSERT INTO ratings (movie_id, user_id, stars, review) VALUES ($movieId, $userId, $stars, $review)"
+
+        for {
+          _ <- db.run(updateQuery)
+          _ <- db.run(insertReviewQuery)
+        } yield Ok(Json.obj("message" -> "Added new rating and review"))
+
+      case None =>
+        Future.successful(NotFound(Json.obj("message" -> "There isn't such film!")))
     }
   }
 
@@ -113,21 +133,6 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     val maybeGivenLogin = (json \ "login").asOpt[String]
     val maybeGivenPassword = (json \ "password").asOpt[String]
 
-    def validateCredentials(login: String, password: String): Future[Boolean] = {
-      val validLength = login.length >= 8 && password.length >= 8
-      if (!validLength) {
-        Future.successful(false)
-      } else {
-        val query = sql"SELECT firstname FROM users WHERE login = $login".as[String]
-        db.run(query.headOption).map {
-          case Some(_) =>
-            false
-          case _ =>
-            true
-        }
-      }
-    }
-
     (maybeGivenFirstName, maybeGivenLastnName, maybeGivenLogin, maybeGivenPassword) match {
       case (Some(givenFirstName), Some(givenLastName), Some(givenLogin), Some(givenPassword)) =>
         validateCredentials(givenLogin, givenPassword).flatMap { isValid =>
@@ -143,6 +148,21 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
         }
       case _ =>
         Future.successful(BadRequest(Json.obj("message" -> "Nieprawidłowe dane do rejestracji")))
+    }
+  }
+
+  def validateCredentials(login: String, password: String): Future[Boolean] = {
+    val validLength = login.length >= 8 && password.length >= 8
+    if (!validLength) {
+      Future.successful(false)
+    } else {
+      val query = sql"SELECT firstname FROM users WHERE login = $login".as[String]
+      db.run(query.headOption).map {
+        case Some(_) =>
+          false
+        case _ =>
+          true
+      }
     }
   }
 
