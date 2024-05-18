8226e8f15bfb7d0701f40608dcd1b06a51c8bd41
mateuszlampert <mateuszlampert@gmail.com>
2024-05-18 02:39:18 +0200

    movies list/chips styling

--- frontend/filmweb/src/main/scala/filmweb/components/MovieChip.scala
+++ frontend/filmweb/src/main/scala/filmweb/components/MovieChip.scala
@@ -2,14 +2,39 @@ package movieChip
 
 import com.raquo.laminar.api.L.{*, given}
 import models.Movie
+import org.scalajs.dom.console.log
+import org.scalajs.dom.window._
 
 class MovieChip(movie: Movie) {
     def renderMovieChip(): Element = {
-
         div(
-            p({movie.movieName}),
-            p({movie.movieId})
+            cls := "movie-chip",
+            p(
+                cls := "movie-name",
+                {movie.movieName}
+            ),
+            p(
+                cls := "movie-rating",
+                s"Rating: ${movie.averageRating}"
+            ),
+            p(
+                cls := "movie-rating",
+                s"Short description: ${movie.shortDescription match {
+                    case Some(value) => value
+                    case _ => "No description provided..."
+                }}"
+            ),
+            p(
+                cls := "movie-category",
+                s"Category: ${movie.category}"
+            ),
+            button(
+                typ("button"),
+                "Details",
+                onClick --> { _ =>
+                   location.href = s"http://localhost:5173/details/${movie.movieId}"
+                }
+            )
         )
     }
 }
-
