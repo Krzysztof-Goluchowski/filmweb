9073023a164c4f026f5aa4081f08500cb33c226d
mateuszlampert <mateuszlampert@gmail.com>
2024-06-04 20:22:53 +0500

    Movies component refactorization, replaced ajax with fetch method

--- frontend/filmweb/src/main/scala/filmweb/components/Movies.scala
+++ frontend/filmweb/src/main/scala/filmweb/components/Movies.scala
@@ -1,71 +1,107 @@
-package movies
+package components
 
 import com.raquo.laminar.api.L.{*, given}
-import org.scalajs.dom.console.log
+import org.scalajs.dom.console.{log}
+import org.scalajs.dom.window.{alert}
 import scala.scalajs.js
 import upickle.default._
 import com.raquo.laminar.api.features.unitArrows
-import movieChip.MovieChip
-import models._
-
-val allCategories = (Category.values.map(category => category.toString) :+ "").sorted
+import components.MovieChip._
+import models.responses._
+import models.movies._
+import io.laminext.fetch.upickle._
+import scala.concurrent.ExecutionContext.Implicits.global
+import components.implicits.{rw, owner}
 
 object Movies {
+    val fetchedCategoriesVar = Var(Seq.empty[String])
+    val fetchedMoviesVar = Var(Seq.empty[MovieDetails])
 
-  val categoryVar = Var(Option.empty[String])
-
-  case class FetchOption(name: String, baseUrl: String, bustCache: Boolean = false) {
-    def id: String = "fetch-" + name
-    def url: String = if (bustCache) baseUrl + "?t=" + js.Date.now() else baseUrl
-  }
-
-  private val options = List(
-    FetchOption("Valid Fetch request", "http://localhost:9000/movies"),
-  )
-
-  def movies(): Element = {
-    val selectedOptionVar = Var(options.head)
-    val eventsVar = Var(Seq.empty[Movie])
-
-    form(
-      p(
-        cls := "category-select",
-        label("Categories: "),
-        select(
-         onChange.mapToValue.map(value => if (value == "") None else Some(value)) --> categoryVar,
-          value <-- categoryVar.signal.map(_.getOrElse("")),
-          allCategories.map(category =>
-            option(
-              value(category),
-              category
-            )
-          )
-        )
-      ),
-      div(
-        button(
-          typ("button"),
-          "Browse",
-          inContext { thisNode =>
-            val clicks = thisNode.events(onClick).sample(selectedOptionVar.signal)
-            val responses = clicks.flatMap { opt =>
-              FetchStream.get(url = opt.url)
-                .map(resp => read[Seq[Movie]](resp))
-                .map(resp => resp.filter(movie => categoryVar.now().isEmpty || categoryVar.now().contains(movie.category)))
-                .recover { case err: Throwable => None }
+    def fetchCategories(): Unit = {
+        Fetch.get(
+            url = "http://localhost:9000/categories"
+        ).decodeEither[ErrorResponse, Seq[String]]
+        .foreach { 
+            response => {
+                response.data match {
+                    case Left(error) => {
+                        alert(error.message)
+                    }
+                    case Right(categories) => {
+                        log(categories.toString())
+                        fetchedCategoriesVar.set((categories :+ "---").sorted)
+                    }
+                }
             }
+        }
+    }
 
-            responses --> { movies =>
-              eventsVar.set(movies)
+    def fetchMovies(url: String, category: Option[String] = None): Unit = {
+        Fetch.get(
+            url = url
+        ).decodeEither[ErrorResponse, Seq[MovieDetails]]
+        .foreach { 
+            response => {
+                response.data match {
+                    case Left(error) => {
+                        alert(error.message)
+                    }
+                    case Right(movies) => {
+                        val filteredMovies = category match {
+                            case Some(categoryToFilter) => {
+                                movies.filter(movie => categoryToFilter == movie.category)
+                            }
+                            case None => {
+                                movies
+                            }
+                        }
+                        log(filteredMovies.toString())
+                        fetchedMoviesVar.set(filteredMovies)
+                    }
+                }
             }
-          }
+        }
+    }
+
+    def renderMovies(url: String): Element = {
+        val categoryVar = Var(Option.empty[String])
+        fetchCategories();
+
+        form(
+            p(
+                cls := "category-select",
+                label("Categories: "),
+                select(
+                    onChange.mapToValue.map(value => if (value == "---") None else Some(value)) --> categoryVar,
+                    value <-- categoryVar.signal.map(_.getOrElse("---")),
+                    children <-- fetchedCategoriesVar.signal.map { 
+                        categories =>
+                        categories.map { 
+                            category =>
+                                option(
+                                    value(category),
+                                    category
+                                )
+                        }
+                    }
+                )
+            ),
+            div(
+                button(
+                    typ("button"),
+                    "Browse",
+                    onClick --> {categoryVar.now() match {
+                            case Some(category) => fetchMovies(url, Some(category))
+                            case None => fetchMovies(url)
+                        }
+                    }
+                )
+            ),
+            div(
+                fontSize.em(0.8),
+                br(),
+                div(children <-- fetchedMoviesVar.signal.map(_.map(el => MovieChip.renderMovieChip(el))))
+            ),
         )
-      ),
-      div(
-        fontSize.em(0.8),
-        br(),
-        div(children <-- eventsVar.signal.map(_.map(el => new MovieChip(el).renderMovieChip())))
-      ),
-    )
-  }
+    }
 }
